589c8839e6a0d8ae5e3b4f6e48c095d5bdc945b3
Samvel Avanesov <samvel.avanesov@oregan.net>
2024-05-31 12:01:46 -0400

    delete auto gen js files

--- src/utils/time/time.js
+++
@@ -1,8 +0,0 @@
-export const TIME_FORMAT = {
-    hour: '2-digit',
-    minute: 'numeric',
-    second: '2-digit',
-    fractionalSecondDigits: 3,
-    hourCycle: 'h23',
-};
-export const now = () => new Date().toLocaleTimeString('en-US', TIME_FORMAT);
