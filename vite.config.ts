368b071c2b5076333f1e62d34158068e8c9ce4f3
Sam Avanesov <sam.avanesoff@gmail.com>
2024-05-30 21:15:57 -0400

    Initial setup, ts, es, pret, vite

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite'; 
+import path from 'path';
+import { fileURLToPath } from 'url';
+import tsconfigPaths from 'vite-tsconfig-paths'; 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'image-cache',
+    },
+    sourcemap: true, // Enable source maps
+    rollupOptions: {
+      external: ['react', 'react-dom', 'tslib'],
+      output: [
+        {
+          format: 'es',
+          entryFileNames: '[name].js',
+          dir: 'dist/esm',
+          sourcemap: true,
+        },
+        {
+          format: 'cjs',
+          entryFileNames: '[name].js',
+          dir: 'dist/cjs',
+          sourcemap: true,
+        },
+      ],
+    },
+  },
+  plugins: [tsconfigPaths()],
+});
